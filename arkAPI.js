42a5f56196f94290c13e62a41cc5046fa48a6a6c
Scott Yamamoto <scottyamamoto@Scotts-MacBook-Pro.local>
2018-12-04 08:53:30 -0600

    I think I did something again

--- arkAPI.js
+++ arkAPI.js
@@ -45,33 +45,35 @@ var oarrAdd = function(item){
   this.publish('change/add', { index : idx, value : item });
 };
 
-var model = new Observable({
+var model = (function(){ 
+  var _ = new Observable({
   version : 0,
-  news : "",
-  serverStatus : "",
-  servers : new Observe([]),
-});
-Object.defineProperty(model, 'majorVersion', {get(){ return parseInt(this.version).toFixed(0); }}); //Can only retrieve initial version of version otherwise, for some reason.
-//Array.push/pop/shift/unshift doesn't trigger change
-model.servers.add = oarrAdd;
+    news : "",
+    serverStatus : "",
+    servers : new Observe([]),
+  });
+  Object.defineProperty(_, 'majorVersion', {get(){ return parseInt(this.version).toFixed(0); }}); //Can only retrieve initial version of version otherwise, for some reason.
+  _.servers.add = oarrAdd;   //Array.push/pop/shift/unshift doesn't trigger change
+  
+  return _;
+})();
 
-var _Server = function(configuration = {}){
-  if(!(this instanceof _Server)){
+
+
+var Server = function(configuration = {}){
+  if(!(this instanceof Server)){
     throw new Error("Server is a constructor and must be called with the new keyword.");
   }
   var setDefault = (val, defaultValue) => { return val === undefined ? defaultValue : val; }
-  var query = (function(hasConfig){
-    return {
-	  address : hasConfig ? setDefault(configuration.query.address, null) : null,
-	  port : hasConfig ? setDefault(configuration.query.port, null) : null,
+  var query = {
+	  address : configuration.hasOwnProperty("query") ? setDefault(configuration.query.address, null) : null,
+	  port : configuration.hasOwnProperty("query") ? setDefault(configuration.query.port, null) : null,
 	};
-  })(configuration.hasOwnProperty("query"));
-  
   
   this.name = setDefault(configuration.name, null);
-  this.address = (query.address && query.port) ? (query.address + ":" + port) : null;
+  this.address = (query.address && query.port) ? (query.address + ":" + query.port) : null;
   this.map = setDefault(configuration.map, null);
-  this.players = setDefault(configuration.players, []);
+  this.players = setDefault(configuration.players, []).map(x=> x.name);
   Object.defineProperty(this, 'playerCount', { get(){ return this.players.length; } });
   this.maxPlayers = setDefault(configuration.maxplayers, null);
   this.rules = (function(hasRules){
@@ -91,47 +93,9 @@ var _Server = function(configuration = {}){
   })(configuration.hasOwnProperty("raw") && configuration.raw.hasOwnProperty("rules"));
 }
 
-var view = (function(){
-  var ui = {
-    navItems : document.querySelectorAll('.nav-item[data-index]'),
-	tabs : document.querySelectorAll('.tab[data-index]'),
-	version : document.querySelector('#version'),
-	majorVersion : document.querySelector('#majorVersion'),
-	serverStatus : document.querySelector('#serverStatus'),
-	news : document.querySelector('#news'),
-	loadingSpan : document.querySelector(".loadingSpan"),
-	servers: document.querySelector('#hosts'),
-	filters : {
-	  maps : document.querySelectorAll('#maps input[type="checkbox"]'),
-	  modes: document.querySelectorAll('#gameModes input[type="checkbox"]'),
-	  custom: document.querySelector('#txtFind'),
-	},
-	serverInfo : {
-	  name : document.querySelector('#serverName'),
-	  clusterID : document.querySelector('#clusterID'),
-	  serverLink : document.querySelector('#serverLink'),
-	  hasPassword : document.querySelector('#passwordRequired'),
-	  map : document.querySelector('#map'),
-	  isPvP : document.querySelector('#isPvP'),
-	  isOfficial : document.querySelector('#isOfficial'),
-	  hasBattleEye : document.querySelector('#usesBattleEye'),
-	  playerCount : document.querySelector('#playerCount'),
-	  players : document.querySelector('#players'),
-	  hasCharacterDownloads : document.querySelector('#playerDownloadsAllowed'),
-	  hasItemDownloads : document.querySelector('#itemDownloadsAllowed'),
-	  time : document.querySelector('#daytime'),
-	  timeout : document.querySelector('#timeout'),
-	},
-  };
-
-  
-  
-  return ui;
-})();
-
 var viewModel = (()=>{
   var _vm = new Observable({
-    tabID : 0,
+    tabID : -1,
 	serverID : 0,
 	servers : new Observe([]),
 	hosts : [],
@@ -155,8 +119,8 @@ var viewModel = (()=>{
 	      original = filters.maps.length ? original.filter((server) => filters.maps.includes(server.map)) : original;
 		  break;
 	    case 'modes':
-	      var pveServers = (filters.modes.includes('pve')) ? original.filter((server) => { return Boolean(server.name.match(/pve/gi)) || !server.rules.isPvP; }) : [],
-	  	    pvpServers = (filters.modes.includes('pvp')) ? original.filter((server) => { return Boolean(server.name.match(/pvp/gi)) || server.rules.isPvP; }) : [],
+	      var pveServers = (filters.modes.includes('pve')) ? original.filter((server) => { return Boolean(server.name.match(/pve/gi)) || !server.rules.isPvE; }) : [],
+	  	    pvpServers = (filters.modes.includes('pvp')) ? original.filter((server) => { return Boolean(server.name.match(/pvp/gi)) || server.rules.isPvE; }) : [],
 	  		notameServers = (filters.modes.includes('noTame')) ? original.filter((server) => server.name.includes("NoTaming")) : [];
 	      var filteredServers = pveServers
 	  						  .concat(pvpServers)
@@ -186,8 +150,8 @@ var viewModel = (()=>{
 		  var fPvP = false,
 		      fPvE = false,
 			  fNoT = false;
-		  if(filters.modes.includes('pvp')){ fPvP = (server.rules.isPvP !== '---') ? server.rules.isPvP : Boolean(server.name.match(/pvp/gi));}	  
-		  if(filters.modes.includes('pve')){ fPvE = (server.rules.isPvP !== '---') ? !server.rules.isPvP : Boolean(server.name.match(/pve/gi));}
+		  if(filters.modes.includes('pvp')){ fPvP = (server.rules.isPvE) ? !server.rules.isPvE : Boolean(server.name.match(/pvp/gi));}	  
+		  if(filters.modes.includes('pve')){ fPvE = (server.rules.isPvE) ? server.rules.isPvE : Boolean(server.name.match(/pve/gi));}
 		  if(filters.modes.includes('noTame')){ fNoT = server.name.includes('NoTaming'); }
 		  
 		  return (filters.modes.length) ? (fPvP || fPvE || fNoT) : true;
@@ -197,14 +161,14 @@ var viewModel = (()=>{
   };
   
   //Model/VM Change events
-  _vm.subscribe('change/tabID', (tabID) => displayTab(tabID.new));
-  _vm.subscribe('change/serverID', (serverID) => displayServerInfo(serverID.new));
-  _vm.servers.subscribe('change', (server) => addServersToList(server.new)); //{ old: [], new : [Server,Server,Server]}
-  _vm.subscribe('change/hosts', (hosts) => queryServers(hosts.new));
+  _vm.subscribe('change/tabID', (tabID) => vmEvents.displayTab(tabID.new));
+  _vm.subscribe('change/serverID', (serverID) => vmEvents.displayServerInfo(serverID.new));
+  _vm.servers.subscribe('change', (server) => vmEvents.addServersToList(server.new)); //{ old: [], new : [Server,Server,Server]}
+  _vm.subscribe('change/hosts', (hosts) => vmEvents.queryServers(hosts.new));
   
-  model.subscribe('change/version', (version) => updateVersion(version.new));
-  model.subscribe('change/news', (news) => displayNews(news.new));
-  model.subscribe('change/serverStatus', (serverStatus) => displayServerStatus(serverStatus.new));
+  model.subscribe('change/version', (version) => vmEvents.updateVersion(version.new));
+  model.subscribe('change/news', (news) => vmEvents.displayNews(news.new));
+  model.subscribe('change/serverStatus', (serverStatus) => vmEvents.displayStatus(serverStatus.new));
   model.servers.subscribe('change/add', (server) => _vm.servers(_vm.servers().concat(server.value)));// model -> filter -> vm -> ui
   
   filters.subscribe('change/maps', (map) => filters.update('maps'));
@@ -212,120 +176,118 @@ var viewModel = (()=>{
   filters.subscribe('change/custom', (custom) => filters.update('custom'));
   
   //UI events
-  var changeTabIndex = function(){  _vm.tabID = this.dataset.index; };
+  var changeTabIndex = function(){ _vm.tabID = this.dataset.index; };
   var setServerID = function(){ _vm.serverID = this.dataset.name; };
-  
-  //Display tab 1 and hide the others.
-  (()=>{
-    Array.from(ui.tabs).forEach(x=> x.classList.add("hidden"));
-    Array.from(ui.tabs)[0].classList.remove("hidden");
-  })();
+  //Map/Mode checkbox click events
+      //Add map or filter out non-matches. Consider just setting filters to the map | mode?
+  var mapFilter = function(){ filters.maps = (this.checked) ? filters.maps.concat(this.value) : filters.maps.filter(condition => condition != this.value); },
+      //Add mode or filter out non-matches.
+      modeFilter = function(){ filters.modes = (this.checked) ?  filters.modes.concat(this.value) : filters.modes.filter(condition => condition != this.value); };
   
   //DOM event binding
-  Array.from(ui.navItems).forEach((navItem) => navItem.onclick = changeTabIndex);
-  var mapFilter = function(){
-    if(this.checked){ filters.maps = filters.maps.concat(this.value); } 
-    else { filters.maps = filters.maps.filter(condition => condition != this.value); }//remove
-  },
-      modeFilter = function(){
-  	if(this.checked){ filters.modes = filters.modes.concat(this.value); } 
-      else { filters.modes = filters.modes.filter(condition => condition != this.value); }
-  };
-  Array.from(ui.filters.maps).forEach(x=> x.onclick = mapFilter);
-  Array.from(ui.filters.modes).forEach(x=> x.onclick = modeFilter);
-  ui.filters.custom.oninput = (e) => { filters.custom = e.target.value; };
+  Array.from(view.navItems).forEach((navItem) => navItem.onclick = changeTabIndex);  
+  Array.from(view.filters.maps).forEach(x=> x.onclick = mapFilter);
+  Array.from(view.filters.modes).forEach(x=> x.onclick = modeFilter);
+  view.filters.custom.oninput = (e) => { filters.custom = e.target.value; };
+   
+   var sanitizer = {
+     status : function(status){  
+	   return status.split(rsc.utils.regex.newlines)//remove newlines and creates distinct entries
+	                .filter(item => item.trim())    //removes empty entries created by split
+	                .map(item => item.replace(rsc.utils.regex.tags, ""));//removes ark's custom xml markup
+	 },
+	 news : function(news){  //Honestly same as above but I don't trust the guys who manage the endpoint to be consistent. They've changed newlines to double spaces before :/
+	   return news.split(rsc.utils.regex.newlines)
+	                .filter(item => item.trim())
+	                .map(item => item.replace(rsc.utils.regex.tags, ""));
+	 },
+   };
    
   //VM events
-  var displayTab = (tabID) => {
-    var tabs = Array.from(ui.tabs),
-	    selectedTab = tabs.find((tab)=> tab.dataset.index == tabID);
-    if(selectedTab){
-	  tabs.forEach((tab)=> tab.classList.add('hidden'));
-	  selectedTab.classList.remove('hidden');
-	}	
-  };
-  var updateVersion = (version) => {
-    ui.version.innerText = version;
-	ui.majorVersion.innerText = parseInt(version).toFixed(0);
-  };
-  var displayServerStatus = (status) => {
-    var statuses = status.split(rsc.utils.regex.newlines),
-	    addStatus = (statusItem) => {
-		  var p = document.createElement('p');
-		  p.innerText = statusItem;
-		  ui.serverStatus.appendChild(p);
-		};
+  var vmEvents = {
+    updateVersion(version){
+	  view.version.innerText = version;
+	  view.majorVersion.innerText = parseInt(version).toFixed(0);
+	},
+    displayTab(tabID){
+	  var tabs = Array.from(view.tabs),
+	      selectedTab = tabs.find((tab)=> tab.dataset.index == tabID);
+      if(selectedTab){
+	    tabs.forEach((tab)=> tab.classList.add('hidden'));
+	    selectedTab.classList.remove('hidden');
+	  }	
+	},
+	//Tab 0
+	displayStatus(status){
+      var statuses = sanitizer.status(status);
+	  addStatus = (statusItem) => { view.serverStatus.appendChild(createP(statusItem)); };
 		
-	statuses = statuses.filter(x => x.trim() != "").map(y => y.replace(rsc.utils.regex.tags,""));
-	statuses.forEach(addStatus);
-  };
-  var displayNews = (news) => {
-    var newsItems = news.split(rsc.utils.regex.newlines),
-		addNews = (newsItem) => {
-		  var p = document.createElement('p');
-		  p.innerText = newsItem;
-		  ui.news.appendChild(p);
-		};
-			
-	newsItems = newsItems.filter(x => x.trim() != "").map(y => y.replace(rsc.utils.regex.tags,""));
-	newsItems.forEach(addNews);
-  };
-  var displayServerInfo = (serverID) => {
-    var server = model.servers().find((server) => server.name == serverID)
-    if(server){  
-	  Object.keys(server).forEach((key) => {  
-	    function mapData(prop, data){
-	      document.querySelector(`[data-prop='${prop}']`).innerText = data;	  
+	  statuses.forEach(addStatus);
+	},
+	displayNews(news){
+	  var newsItems = sanitizer.news(news),
+      addNews = (newsItem) => { view.news.appendChild(createP(newsItem)); };
+
+	  newsItems.forEach(addNews);
+	},
+	//Tab 1
+    addServersToList(serverList){
+      function addServerToList(server){
+        if(filters.test(server)){ 
+	      view.servers.appendChild(createLI(server));
 	    }
-	    if(key === 'rules'){
-	      Object.keys((server.rules)).forEach((rule) => { mapData(rule, server.rules[rule]); });
-	    } else {
-	      mapData(key, server[key]);
+	  }
+      
+      serverList.sort((a,b) => a.name.localeCompare(b.name,"en-US-u-kn-true"));
+	  
+      var currentList = Array.from(document.querySelectorAll('#hosts li')).map((li)=> li.dataset.name),
+	      newList = serverList.map((server) => server.name);
+	  		
+	  var additions = newList.filter((id) => { return !currentList.includes(id) && filters.test(serverList.find((server) => server.name == id));}),
+	      removals = currentList.filter((id) => !newList.includes(id));
+	  	
+	  var parent = view.servers;
+	  
+	  additions.forEach((id) => {
+	    if(!parent.hasChildNodes()){ addServerToList( serverList.find((server) => server.name == id) ); }
+	    else {
+	      var idx = newList.indexOf(id),
+	          prevElement = Array.from(parent.childNodes).find((element) => element.dataset.name == newList[idx - 1]);// doesn't work bc new list will have things in a different order
+	    			
+	      if(prevElement == null){ parent.insertBefore(createLI(serverList[idx]), parent.firstChild); }
+	      else { parent.insertBefore(createLI(serverList[idx]), prevElement.nextSibling); }
 	    }
 	  });
+	  removals.forEach((id)=> parent.removeChild(parent.querySelector(`li[data-name='${id}']`)));
+    },
+	queryServers(ipList){  	
+	  document.querySelector(".loadingSpan").classList.add("loading");
+	  loadServers();
+    },
+	//Tab 2
+	displayServerInfo(serverID){
+	  var server = model.servers().find((server) => server.name == serverID)
+      if(server){  
+	    console.log(server.players);
+	    Object.keys(server).forEach((key) => {  
+	      function mapData(prop, data){
+		    try {
+	          document.querySelector(`[data-prop='${prop}']`).innerText = data;	  
+			} catch(err){console.log(prop); }
+	      }
+	      if(key === 'rules'){
+	        Object.keys((server.rules)).forEach((rule) => { mapData(rule, server.rules[rule]); });
+	      } else {
+	        mapData(key, server[key]);
+	      }
+	    });
 	  	  
-	  _vm.tabID = 2;
-	} else {
-	  throw new Error("Server not found.");
-	}
-  }
-  var queryServers = (ipList) => {  	
-	document.querySelector(".loadingSpan").classList.add("loading");
-	loadServers();
-  };
-  var addServerToList = (server) => {	
-	if(filters.test(server)){ 
-	  ui.servers.appendChild(createLI(server));
-	}
-	else { console.log("You've lost the game");}
-  };
-  var addServersToList = (serverList) => {
-    serverList.sort((a,b) => a.name.localeCompare(b.name,"en-US-u-kn-true"));
-	//console.log('sl:', serverList);
-	
-    var currentList = Array.from(document.querySelectorAll('#hosts li')).map((li)=> li.dataset.name),
-	    newList = serverList.map((server) => server.name);
-			
-	var additions = newList.filter((id) => { return !currentList.includes(id) && filters.test(serverList.find((server) => server.name == id));}),
-	    removals = currentList.filter((id) => !newList.includes(id));
-		
-	var parent = ui.servers;
-	
-	//console.log('additions:',additions);
-	//console.log('removals: ', removals.length);
-	
-	additions.forEach((id) => {
-	  if(!parent.hasChildNodes()){ addServerToList( serverList.find((server) => server.name == id) ); }
-	  else {
-	    var idx = newList.indexOf(id),
-	        prevElement = Array.from(parent.childNodes).find((element) => element.dataset.name == newList[idx - 1]);// doesn't work bc new list will have things in a different order
-	  			
-	    if(prevElement == null){ parent.insertBefore(createLI(serverList[idx]), parent.firstChild); }
-	    else { parent.insertBefore(createLI(serverList[idx]), prevElement.nextSibling); }
+	    _vm.tabID = 2;
+	  } else {
+	    throw new Error("Server not found.");
 	  }
-	});
-	removals.forEach((id)=> parent.removeChild(parent.querySelector(`li[data-name='${id}']`)));
-  };
+	},
+  };  
   
   //Utility
   var createLI = (server) => {
@@ -336,21 +298,27 @@ var viewModel = (()=>{
 	
 	return li;
   };
+  var createP = (description) => {
+    var p = document.createElement("p");
+	p.innerText = description;
+	return p;
+  };
   
   var loadServers = () => {
     if(!_vm.hosts.length){
-	  ui.loadingSpan.classList.remove('loading');
-	  ui.loadingSpan.classList.add('done');
-	} else {
-	  var ip = _vm.hosts.shift();
-	  rsc.serverInfo(ip).then(x=>{
-	    //vm.data.servers.sort((y,z) => y.name.localeCompare(z.name,"en-US-u-kn-true"));
-		model.servers.add(new Server(x.value));
-	  })
-	  .catch((err)=>{})
-	  .then(()=> loadServers());	
+	  view.loadingSpan.classList.remove('loading');
+	  view.loadingSpan.classList.add('done');
+	  return;
 	}
+	var ip = _vm.hosts.shift();
+	rsc.serverInfo(ip).then(x=>{
+	  model.servers.add(new Server(x.value));
+	})
+	.catch((err)=>{  }) //Any kind of miscellaneous error could have occurred. Usually times out internally though. (408 Request Timeout)
+	.then(()=> loadServers()); //Recurse
   };
+  
+  _vm.tabID = 0;
   
   return _vm;
 })();  
@@ -369,7 +337,6 @@ var viewModel = (()=>{
   promiseTimer(rsc.version(), 60000, "Version info failed to load.")
 	.then(x=> model.version = x.value)
 	.catch(x=> {
-	  console.log(x); 
 	  document.querySelector('#version').innerText = "unavailable";
 	  document.querySelector('#majorVersion').innerText = "unavailable";
 	});
@@ -388,4 +355,4 @@ var viewModel = (()=>{
 		if(ip){ y.push(z.match(rsc.utils.regex.ip)[0]); }
 		return y;
 	  }, []);
-  }).catch(a=> {console.log(a); alert("Server list unavailable."); });
+  }).catch(a=> {console.log(a); alert("Server list unavailable."); });	
